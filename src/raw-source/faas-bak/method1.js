f216c8564cdfccbf947b88bade98c37ca881d1c2
Jiyu Shao <jiyu.shao@gmail.com>
2024-05-07 16:15:45 +0800

    move faas-bak to faas

--- src/raw-source/faas-bak/method1.js
+++
@@ -1,6 +0,0 @@
-import { log } from '/faas/common.js';
-
-export async function fetch(params, context) {
-  log('log from method1');
-  return 'method1';
-}
--- src/raw-source/faas-bak/method2.js
+++
@@ -1,3 +0,0 @@
-export default async function fetch (params, context) {
-  return 'method2';
-}
